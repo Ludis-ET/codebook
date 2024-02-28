65af916e4d287ebb39e86fdf0dfb95e2858e3885
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-02-28 15:01:53 +0300

    Header component updated with Flowbite navigation bar and contact details. Dark mode added.

--- src/components/Layouts/Header.jsx
+++ src/components/Layouts/Header.jsx
@@ -1,5 +1,37 @@
 export const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+          <a
+            href="https://flowbite.com"
+            className="flex items-center space-x-3 rtl:space-x-reverse"
+          >
+            <img
+              src="https://flowbite.com/docs/images/logo.svg"
+              className="h-8"
+              alt="Flowbite Logo"
+            />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              Flowbite
+            </span>
+          </a>
+          <div className="flex items-center space-x-6 rtl:space-x-reverse">
+            <a
+              href="tel:5541251234"
+              className="text-sm  text-gray-500 dark:text-white hover:underline"
+            >
+              (555) 412-1234
+            </a>
+            <a
+              href="#"
+              className="text-sm  text-blue-600 dark:text-blue-500 hover:underline"
+            >
+              Login
+            </a>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
