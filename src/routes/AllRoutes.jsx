4cbabd98e0b49e573ca26550e19deaedd9bb92c7
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-02-28 15:43:10 +0300

    Changed the import order of Route and Routes. Added a ProductsList page. Changed the Route component to use the forward slash syntax

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -1,12 +1,13 @@
-import { Route, Routes } from "react-router-dom"
-import { HomePage } from "../pages";
+import { Routes, Route } from "react-router-dom";
+import { HomePage, ProductsList } from "../pages";
 
 export const AllRoutes = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<ProductsList />} />
       </Routes>
     </>
   );
-}
+};
