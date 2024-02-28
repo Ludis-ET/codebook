0b28d2afba56fe8952a573a8543055d825cb9a64
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-02-28 15:22:44 +0300

    Update dependencies and add Bootstrap icons

--- src/components/Layouts/Header.jsx
+++ src/components/Layouts/Header.jsx
@@ -1,34 +1,28 @@
+import { Link } from "react-router-dom";
+import Logo from "../../assets/logo.png";
+
 export const Header = () => {
   return (
     <header>
-      <nav className="bg-white border-gray-200 dark:bg-gray-900">
-        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
-          <a
-            href="https://flowbite.com"
-            className="flex items-center space-x-3 rtl:space-x-reverse"
-          >
-            <img
-              src="https://flowbite.com/docs/images/logo.svg"
-              className="h-8"
-              alt="Flowbite Logo"
-            />
+      <nav className="bg-white dark:bg-gray-900">
+        <div className="border-b border-slate-200 dark:border-b-0 flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-3">
+          <Link to="/" className="flex items-center">
+            <img src={Logo} className="mr-3 h-10" alt="CodeBook Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Flowbite
+              CodeBook
             </span>
-          </a>
-          <div className="flex items-center space-x-6 rtl:space-x-reverse">
-            <a
-              href="tel:5541251234"
-              className="text-sm  text-gray-500 dark:text-white hover:underline"
-            >
-              (555) 412-1234
-            </a>
-            <a
-              href="#"
-              className="text-sm  text-blue-600 dark:text-blue-500 hover:underline"
-            >
-              Login
-            </a>
+          </Link>
+          <div className="flex items-center relative">
+            <span className="cursor-pointer text-xl text-gray-700 dark:text-white mr-5 bi bi-gear-wide-connected"></span>
+            <span className="cursor-pointer text-xl text-gray-700 dark:text-white mr-5 bi bi-search"></span>
+            <Link to="/cart" className="text-gray-700 dark:text-white mr-5">
+              <span className="text-2xl bi bi-cart-fill relative">
+                <span className="text-white text-sm absolute -top-1 left-2.5 bg-rose-500 px-1 rounded-full ">
+                  0
+                </span>
+              </span>
+            </Link>
+            <span className="bi bi-person-circle cursor-pointer text-2xl text-gray-700 dark:text-white"></span>
           </div>
         </div>
       </nav>
