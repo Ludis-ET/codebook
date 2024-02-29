d233f58a10ccb7ebbd7b813e4e3c8bd24c8db718
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-02-29 08:01:58 +0300

    Remove the `serach` parameter from the `useFetch` hook

--- src/hooks/useSearch.jsx
+++ src/hooks/useSearch.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-export const useFetch = (url) => {
+export const useFetch = (url, serach) => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
