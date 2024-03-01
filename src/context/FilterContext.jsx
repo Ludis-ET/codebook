19c0732eda1bc411195236dc93b1e14587ae8a05
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-03-01 11:58:35 +0300

    Added functionality to filter products by best-seller, sort by price, and filter by product rating.

--- src/context/FilterContext.jsx
+++ src/context/FilterContext.jsx
@@ -16,14 +16,39 @@ export const FilterProvider = ({ children }) => {
   function initialProductList(products) {
     dispatch({
       type: "PRODUCT_LIST",
-      payload:{
-        products:products
-      }
+      payload: {
+        products: products,
+      },
     });
   }
-  function bestSeller(products){
-    return products.filter(product => product.best_seller === true)
+  function bestSeller(products) {
+    return products.filter((product) => product.best_seller === true);
   }
+
+  function sort(products) {
+    if (state.sortBy === "lowtohigh") {
+      return products.sort((a, b) => Number(a.price) - Number(b.price));
+    }
+    if (state.sortBy === "hightolow") {
+      return products.sort((b, a) => Number(a.price) - Number(b.price));
+    }
+  }
+
+  function rating(products){
+    if(state.ratings === "4STARSABOVE"){
+        return products.filter(product => product.rating >= 4);
+    }
+    if(state.ratings === "3STARSABOVE"){
+        return products.filter(product => product.rating >= 3);
+    }
+    if(state.ratings === "2STARSABOVE"){
+        return products.filter(product => product.rating >= 2);
+    }
+    if(state.ratings === "1STARSABOVE"){
+        return products.filter(product => product.rating >= 1);
+    }
+    return products;
+}
   const value = {
     productList: state.productList,
     initialProductList,
